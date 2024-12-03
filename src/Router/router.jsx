f27db20748c5eab6009b479f73e4be8e66b9a64c
safin33221 <saifn33221@gmail.com>
2024-12-03 18:51:27 +0600

    initilaize setup complete

---
+++ src/Router/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<h2>setup completed</h2>
+    }
+])
+
+export default router;
