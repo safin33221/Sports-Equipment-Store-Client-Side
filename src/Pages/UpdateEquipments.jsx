8b957106c042822b4ef87a3c575bfefdff3856e8
safin33221 <saifn33221@gmail.com>
2024-12-05 14:57:15 +0600

    completed to update equipment

--- src/Pages/UpdateEquipments.jsx
+++ src/Pages/UpdateEquipments.jsx
@@ -4,11 +4,69 @@ import { useLoaderData } from 'react-router-dom';
 const UpdateEquipments = () => {
     const loadedData = useLoaderData()
 
-    const { image, itemName, categoryName, description, price, rating, customization, processingTime, stockStatus, email, name } = loadedData
+    const { _id,email,name, image, itemName, categoryName, description, price, rating, customization, processingTime, stockStatus } = loadedData
+
+    const handleUpdate = e => {
+        e.preventDefault()
+        const form = e.target
+
+
+        const image = form.image.value
+        const itemName = form.itemName.value
+        const categoryName = form.categoryName.value
+        const description = form.description.value
+        const price = form.price.value
+        const rating = form.rating.value
+        const customization = form.customization.value
+        const processingTime = form.processingTime.value
+        const stockStatus = form.stockStatus.value
+
+        const updateProductDetails = { image, itemName, categoryName, description, price, rating, customization, processingTime, stockStatus }
+
+        fetch(`http://localhost:5000/SportsEquipment/${_id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updateProductDetails)
+        })
+            .then(result => {
+                console.log(result);
+            })
+            .catch(error => console.log(error))
+    }
     return (
         <div className="max-w-2xl mx-auto bg-white p-6 shadow-md rounded-lg">
+            <div>
+                <h1 className='text-2xl font-bold '>User info</h1>
+
+                <div className='flex gap-4 my-4'>
+                    <div className='w-1/2'>
+                        <label className="block text-sm font-medium text-gray-700">User Email</label>
+                        <input
+                            value={email}
+                            type="text"
+                            name="image"
+                            placeholder="Image URL"
+                            className="p-2 border-2  outline-none mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+                            required
+                        />
+                    </div>
+                    <div className='w-1/2'>
+                        <label className="block text-sm font-medium text-gray-700">User Name</label>
+                        <input
+                            value={name}
+                            type="text"
+                            name="image"
+                            placeholder="Image URL"
+                            className="p-2 border-2  outline-none mt-1 block w-full rounded-md border-gray-300 shadow-sm sm:text-sm"
+                            required
+                        />
+                    </div>
+                </div>
+            </div>
             <h2 className="text-2xl font-bold mb-4 text-gray-800">Add New Item</h2>
-            <form
+            <form onSubmit={handleUpdate}
                 className="space-y-4 grid grid-cols-12 gap-3">
                 {/* Image Input */}
                 <div className='col-span-12'>
