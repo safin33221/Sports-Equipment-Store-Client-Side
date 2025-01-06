97c1cfb7379f8e20756739805ae320cf6ea5302f
safin33221 <saifn33221@gmail.com>
2025-01-06 22:39:08 +0600

    fixed the navbar in the top position

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
             })
     }
     return (
-        <div className="navbar w-11/12 mx-auto">
+        <div className="navbar fixed px-20 z-50 backdrop-blur-sm ">
             <div className="navbar-start ">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
